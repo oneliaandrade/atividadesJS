bebe1dba19709d918cc6287154036c4a68cbf12d
oneliaandrade <143622681+oneliaandrade@users.noreply.github.com>
2023-10-19 11:11:19 -0300

    Add files via upload

---
+++ atvFuncao.js
@@ -0,0 +1,18 @@
+function calculo (){
+    console.log(`Bem vindo a calculadora`)
+};
+
+calculo();
+
+function diminuir(num1, num2){
+    return num1 - num2
+};
+
+const res = diminuir(20, 5);
+console.log(`O resultado é ${res}`) ;
+
+const soma = (num3, num4) => num3 + num4;
+const resSoma = soma(5,7);
+console.log(`O resultado é ${resSoma}`) ;
+
+
---
+++ calculadoMedia.js
@@ -0,0 +1,10 @@
+var nota1, nota2, nota3, media;
+
+nota1 = parseFloat(prompt("informe sua primeira nota: "));
+nota2 = parseFloat(prompt("informe sua segunda nota: "));
+nota3 = parseFloat(prompt("informe sua terceira nota: "));
+
+media = (nota1 + nota2 + nota3) /3;
+
+console.log(`Sua média é ${media}`)
+
